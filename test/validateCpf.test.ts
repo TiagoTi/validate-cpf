6943a0da7bde0b1c3c6038cff236249c27d66b0c
tiago.cipriano <tiago.cipriano@luilzalabs.com>
2025-04-27 22:12:18 -0300

    test: cria testes de cobertura

--- test/validateCpf.test.ts
+++ test/validateCpf.test.ts
@@ -1,9 +1,23 @@
 import {validate} from '../src/validateCpf'
-test('deve validar um cpf', () => {
+
+
+test.only.each([
+	['comum', '97456321558', true],
+	['com final zero', '71428793860', true],
+	['com final zero zero', '87748248800', true],
+	['com sepradores', '974.563,215-58', true],
+
+	['tamanho menor 11', '9745632155', false],
+	['tamanho maior 14', '974563215581112', false],
+	['invalido', '97456321551', false],
+
+	// ['valor null', null, false],
+	// ['valor undefined', undefined, false],
+])('deve validar um cpf %s', (_, cpf, expectd) => {
 	// given
-	const cpf = "97456321558"
 	// when
 	const valid = validate(cpf);
 	// then
-	expect(valid).toBe(true);
+	expect(valid).toBe(expectd);
 })
+
