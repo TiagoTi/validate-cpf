d75681b86a45ac394d5dfac96eb239ef91fd1f25
tiago.cipriano <tiago.cipriano@luilzalabs.com>
2025-04-29 10:34:56 -0300

    chore: create config to improve identt of files

---
+++ .nvim.lua
@@ -0,0 +1,8 @@
+
+local vim = vim
+vim.cmd([[
+set tabstop=2               " number of columns occupied by a tab 
+set softtabstop=2           " see multiple spaces as tabstops so <BS> does the right thing
+set shiftwidth=2            " width for autoindents
+set cc=120                  " set an 80 column border for good coding style
+]])
